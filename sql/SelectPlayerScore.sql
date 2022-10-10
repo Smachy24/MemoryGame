daa1cab1ac20f902611d0d4c9bcf6c0c8f831a07
Val <pro.mvaldo@gmail.com>
2022-10-10 15:19:18 +0200

    fix

--- sql/SelectPlayerScore.sql
+++ sql/SelectPlayerScore.sql
@@ -1,5 +1,14 @@
+/*
+On créé la requête permettant de récupérer les scores des utilisateurs sur un Jeu.
+Le résultat est ensuite trié.
+
+*/
+
+
+
 SELECT game_id, difficulty, score, Utilisateur.pseudo
 FROM Score
-JOIN Utilisateur ON player_id.Score = Utilisateur.id
-JOIN Game ON Score.game_id = Game.id
-ORDER BY score, difficulty, Game.id
+JOIN Utilisateur ON score.player_id = Utilisateur.id --Relation entre la table score et utilisateur à l'aide de la clé étrangère player_id 
+JOIN Game ON Score.game_id = Game.id --Relation entre la table score et game à l'aide de la clé étrangère game_id
+ORDER BY score, difficulty, Game.id --Trie par ordre alphabétique et en fonction du score et de la difficulté
+
