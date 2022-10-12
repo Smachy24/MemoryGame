e5f7bb677c593796f3c81b6e72a40292c368fe40
Val <111442096+ValzM@users.noreply.github.com>
2022-10-12 08:57:48 +0200

    Update and rename FilterScoresByGame.sql to FilterScoresByPlayer.sql

--- sql/FilterScoresByPlayer.sql
+++ sql/FilterScoresByPlayer.sql
@@ -6,5 +6,5 @@ SELECT game_id, difficulty, score, Utilisateur.pseudo
 FROM Score
 JOIN Utilisateur ON score.player_id = Utilisateur.id --Relation entre la table score et utilisateur à l'aide de la clé étrangère player_id 
 JOIN Game ON Score.game_id = Game.id --Relation entre la table score et game à l'aide de la clé étrangère game_id
-ORDER BY score, difficulty, Game.name --Trie par ordre alphabétique et en fonction du score et de la difficulté
 WHERE Utilisateur.pseudo = '';
+ORDER BY score, difficulty, Game.name --Trie par ordre alphabétique et en fonction du score et de la difficulté
