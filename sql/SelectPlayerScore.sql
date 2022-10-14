5ebf73c5d204c81f12ec9d13281eccb87d7849ba
Val <111442096+ValzM@users.noreply.github.com>
2022-10-14 13:19:37 +0200

    Order by score DESC

--- sql/SelectPlayerScore.sql
+++ sql/SelectPlayerScore.sql
@@ -10,5 +10,5 @@ SELECT Game.name, difficulty, score, Utilisateur.pseudo
 FROM Score
 JOIN Utilisateur ON score.player_id = Utilisateur.id --Relation entre la table score et utilisateur à l'aide de la clé étrangère player_id 
 JOIN Game ON Score.game_id = Game.id --Relation entre la table score et game à l'aide de la clé étrangère game_id
-ORDER BY score, difficulty, Game.name --Trie par ordre alphabétique et en fonction du score et de la difficulté
+ORDER BY score DESC, difficulty, Game.name --Trie par ordre alphabétique et en fonction du score et de la difficulté
 
