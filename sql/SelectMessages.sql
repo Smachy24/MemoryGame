eacddccb4ceb8edfe53e6dc3b44ad55a05763c44
Val <111442096+ValzM@users.noreply.github.com>
2022-10-14 13:23:48 +0200

    fix DATEDIFF

--- sql/SelectMessages.sql
+++ sql/SelectMessages.sql
@@ -4,6 +4,6 @@ Select all messages in last 24 hours
 
 SELECT *
 FROM message
-WHERE DATEDIFF(message_date, CURRENT_TIMESTAMP())<1
+WHERE DATEDIFF(CURRENT_TIMESTAMP(), message_date)<1
 
 --DATEDIF -> (date 1 - date 2)
