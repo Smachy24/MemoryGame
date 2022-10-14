95dd6a31365e6ff41b8b4f90a8ddd11dd22c13f1
Val <111442096+ValzM@users.noreply.github.com>
2022-10-14 09:41:14 +0200

    Fix game_id => Game.name

--- sql/SelectPlayerScore.sql
+++ sql/SelectPlayerScore.sql
@@ -6,7 +6,7 @@ Le résultat est ensuite trié.
 
 
 
-SELECT game_id, difficulty, score, Utilisateur.pseudo
+SELECT Game.name, difficulty, score, Utilisateur.pseudo
 FROM Score
 JOIN Utilisateur ON score.player_id = Utilisateur.id --Relation entre la table score et utilisateur à l'aide de la clé étrangère player_id 
 JOIN Game ON Score.game_id = Game.id --Relation entre la table score et game à l'aide de la clé étrangère game_id
