ba18c05a8d5060b498c1f29cc368cb0da037c57a
Diego Carvalho <pro.diegocdossantos@gmail.com>
2022-10-14 11:29:57 +0200

    transfo index en php + footer et header en php

---
+++ view/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="logo">
+      The Power Of Memory
+    </div>
+    <nav>
+      <ul>
+        <a href="index.html"><li>ACCUEIL</li></a>
+        <a href="game.html"><li>JEU</li></a>
+        <a href="scores.html"><li>SCORES</li></a>
+        <a href="account.html"><li>MON ESPACE</li></a>
+        <a href="login.html"><li>LOGIN</li></a>
+        <a href="contact.html"><li>NOUS CONTACTER</li></a>
+      </ul>
+    </nav>  
+  </header>
