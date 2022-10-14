d766fa47b9b7d3ff7fcfdd3363bc5d8042fa7f31
Seelyks <114948990+KevinCrdr@users.noreply.github.com>
2022-10-14 09:56:08 +0200

    Ajout de commentaires

--- sql/InsertMessage.sql
+++ sql/InsertMessage.sql
@@ -1,5 +1,5 @@
 -- J'indique avec INSERT INTO que j'ajoute une ligne à la tbale Message
--- La valeur "2" : id, "5":id-game, "6":id_sender, "gg":j'insère un message
+-- La valeur "2" : id, "5":id-game, "6":id_sender, "gg":j'insère un message.
 
 
 INSERT INTO Message
