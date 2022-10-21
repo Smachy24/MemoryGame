02c7af208217b75b05f226027b5330bc9e32592b
Diego Carvalho <pro.diegocdossantos@gmail.com>
2022-10-21 13:36:41 +0200

    docstrings

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -24,57 +24,121 @@ class Bdd{
     
   }
   function getPass()
+  /**
+   * Return the PDO'password
+   * @param : none
+   * @return : String pass -> PDO's password
+   */
   {
     return $this->pass;
   }
   function getHost()
+  /**
+   * Return the PDO'host
+   * @param : none
+   * @return : String host -> PDO's host
+   */
   {
     return $this->host;
   }
 
   function getUser()
   {
+  /**
+   * Return the PDO'user
+   * @param : none
+   * @return : String user -> PDO's user
+   */
     return $this->user;
   }
 
   function getName()
+  /**
+   * Return the PDO'name
+   * @param : none
+   * @return : String name -> PDO's name
+   */
   {
     return $this->name;
   }
 
   function getScores()
+  /**
+   * Return all scores
+   * @param : none
+   * @return : Array scores -> All users'scores
+   */
   {
     return $this->scores;
   }
 
-  function getConnect(){
+  function getConnect()
+  /**
+   * Return the PDO'connexion
+   * @param : none
+   * @return : Object(PDO) connexion -> PDO's connexion
+   */
+  {
     return $this -> connexion;
   }
 
-  function getFilter(){
+  function getFilter()
+  /**
+   * Return all filters
+   * @param : none
+   * @return : String scores -> All users scores's filters
+   */
+  {
     return $this -> filter;
   }
 
-  function addFilter($filter){
+  function addFilter($filter)
+  /**
+   * Add filters to $this -> filter 
+   * @param : String $filter -> filters to add to select request
+   * @return : none
+   */
+  {
 
     $this -> filter.= $filter;
   }
 
-  function resetScores(){
+  function resetScores()
+  /**
+   * Reset displayed scores
+   * @param : none
+   * @return : none
+   */
+  {
     $this->scores = [];
   }
 
   function getMessages()
+  /**
+   * Return all messages
+   * @param : none
+   * @return : Array messages -> All data base's messages
+   */
   {
     return $this->messages;
   }
 
   function addScore($array)
+  /**
+   * Add scores to $this -> scores 
+   * @param : Array $array -> array to add to data base's scores
+   * @return : none
+   */
   {
     array_push($this->scores, $array);
   }
 
   function connect()
+  /**
+   * Do the connexion between data base and php
+   * @param : none
+   * @return: none
+   */
 
   {
     try {
@@ -89,6 +153,11 @@ class Bdd{
 
 
   function selectScore(){
+    /**
+     * Select all scores and display them in scores.php
+     * @param:none
+     * @return:none
+     */
     $sql = "SELECT Game.name, Utilisateur.pseudo, difficulty, score, game_date
     FROM Score
     JOIN Utilisateur ON score.player_id = Utilisateur.id
@@ -106,8 +175,6 @@ class Bdd{
       $sql .= $this -> getFilter(); //Other cases (only order, both and nothing)
     }
     
-
-
     $req = $this -> connexion-> prepare($sql);
     
     $req -> execute();
@@ -123,6 +190,11 @@ class Bdd{
   }
 
   function getConnectedPlayers(){
+    /**
+     * Return sql request that select all users in db
+     * @param: none
+     * @return : String $r -> number of users
+     */
     $sql = "SELECT COUNT(id) FROM Utilisateur" ;
     $req = $this -> connexion-> prepare($sql);
     $req -> execute();
@@ -133,6 +205,11 @@ class Bdd{
   }
 
   function getGamesPlayed(){
+    /**
+     * Return sql request that select all games played
+     * @param: none
+     * @return : String $r -> number of games played
+     */
     $sql = "SELECT COUNT(id) FROM Score";
     $req = $this -> connexion-> prepare($sql);
     $req -> execute();
@@ -143,6 +220,11 @@ class Bdd{
   }
 
   function getBestScore(){
+    /**
+     * Return sql request that select the best score in data base
+     * @param: none
+     * @return : String $r -> best score
+     */
     $sql = "SELECT score FROM Score ORDER BY score DESC LIMIT 1";
     $req = $this -> connexion-> prepare($sql);
     $req -> execute();
@@ -153,6 +235,11 @@ class Bdd{
   }
 
   function getMessageCount(){
+    /**
+     * Return sql request that select all messages in db
+     * @param: none
+     * @return : String $r -> number of messages in db
+     */
     $sql = "SELECT COUNT(id) FROM Message" ;
     $req = $this -> connexion-> prepare($sql);
     $req -> execute();
@@ -163,16 +250,12 @@ class Bdd{
   }
 
 
-
-
-
-
-
-
-
-
-
   function selectUser($userIdentifier)
+  /**
+   * Select a specific user through email
+   * @param: String $userIdentifier -> user'email
+   * @return: Array $userDataArray -> array with user'informations (email, id, pseudo and password)
+   */
   {
     $req = $this->connexion->prepare("
     SELECT *
@@ -194,6 +277,11 @@ class Bdd{
   }
 
   function selectUserById($userIdentifier)
+  /**
+   * Select a specific user through id
+   * @param: int $userIdentifier -> user'id
+   * @return: Array $userDataArray -> array with user'informations (email, id, pseudo and password)
+   */
   {
     $req = $this->connexion->prepare("
     SELECT *
@@ -216,6 +304,12 @@ class Bdd{
 
 
   function updateEmail($newMail, $userId)
+  /**
+   * SQL update email of a user
+   * @param: String $newMail -> new value of email
+   * @param: int $userId -> user'id
+   * @return : none
+   */
   {
     $req = $this->connexion->prepare("
     UPDATE utilisateur
@@ -226,6 +320,12 @@ class Bdd{
   }
 
   function updatePassword($newPassword, $userId)
+  /**
+   * SQL update password of a user
+   * @param: String $newPassword -> new value of password
+   * @param: int $userId -> user'id
+   * @return : none
+   */
   {
     $req = $this->connexion->prepare("
     UPDATE utilisateur
@@ -236,6 +336,11 @@ class Bdd{
   }
 
   function getAllMessages()
+  /**
+   * SQL select all messages
+   * @param: none
+   * @return: none
+   */
   {
     $req = $this->connexion->prepare("
     SELECT *
@@ -256,6 +361,13 @@ class Bdd{
   }
 
   function sendMessage($id_game,$id_sender, $message)
+  /**
+   * SQL insert into Message's table a new message
+   * @param: int $id_game -> game'id
+   * @param: int $id_sender -> user'id
+   * @param: String $message -> message
+   * @return: none
+   */
   {
     $req = $this->connexion->prepare("
     INSERT INTO Message(id_game, id_sender, message)
